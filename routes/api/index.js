339fc6aba18a1a6309706a137a432a2f5e29b270
vieiragcv <95590262+vieiragcv@users.noreply.github.com>
2022-04-03 16:17:30 -0400

    finalized and tested comments routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -6,10 +6,10 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes.js');
-//const voteRoutes = require('./vote-routes.js');
+const commentRoutes = require('./comment-routes.js');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
-//router.use('/votes', voteRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -4,7 +4,7 @@
 
 const sequelize = require('../../config/connection');
 const router = require('express').Router();
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 
 /*---------------------------------------------------------------
 -                         GET /api/posts
@@ -15,6 +15,7 @@ const { Post, User, Vote } = require('../../models');
 router.get('/', (req, res) => {
   console.log('======================');
   Post.findAll({
+    order: [['created_at', 'DESC']],
     attributes: [
       'id',
       'post_url',
@@ -22,8 +23,15 @@ router.get('/', (req, res) => {
       'created_at',
       [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
     ],
-    order: [['created_at', 'DESC']],
     include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
       {
         model: User,
         attributes: ['username']
@@ -52,6 +60,14 @@ router.get('/:id', (req, res) => {
       [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
     ],
     include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
       {
         model: User,
         attributes: ['username']
