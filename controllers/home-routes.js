4f551e764ec44c445aad71ac3cc8894cc42508db
vieiragcv <95590262+vieiragcv@users.noreply.github.com>
2022-04-11 17:58:53 -0400

    generating list of posts in the home page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
+
 router.get('/', (req, res) => {
   Post.findAll({
     attributes: [
@@ -27,7 +28,7 @@ router.get('/', (req, res) => {
     ]
   })
     .then(dbPostData => {
-      // pass a single post object into the homepage template
+
       console.log(dbPostData[0]);
       const posts = dbPostData.map(post => post.get({ plain: true }));
       res.render('homepage', { posts });
