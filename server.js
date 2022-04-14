4d872e9ab238f080cab5eb758bd9ba6cae7c91c0
vieiragcv <95590262+vieiragcv@users.noreply.github.com>
2022-04-14 17:56:11 -0400

    included partials and tests

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
-
-const hbs = exphbs.create({});
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
